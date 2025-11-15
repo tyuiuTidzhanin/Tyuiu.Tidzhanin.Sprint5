a4aab15f744076b7dbb494fdb75ad0da3d82b0e5
Нгаргондо Тиджани <tyuiuTidzhanin@outlook.com>
2025-11-16 01:45:15 +0500

    Task1

--- Tyuiu.Tidzhanin.Sprint5.Task1.V4.Test/DataServiceTest.cs
+++ Tyuiu.Tidzhanin.Sprint5.Task1.V4.Test/DataServiceTest.cs
@@ -16,12 +16,13 @@ namespace Tyuiu.Tidzhanin.Sprint5.Task1.V4.Test
             int stopValue = 5;
             string path = ds.SaveToFileTextData(startValue, stopValue);
 
-            Assert.IsTrue(File.Exists(path));
-
             string fileContent = File.ReadAllText(path).Trim();
+            fileContent = fileContent.Replace("\r", "");
+
             string expected = "-15,44\n-10,93\n-7,22\n-5,04\n0\n-0,3\n2,57\n6,4\n10,04\n12,72\n14,68";
 
             Assert.AreEqual(expected, fileContent);
+
         }
     }
 }
